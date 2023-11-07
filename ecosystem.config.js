0a6a5f03c84838c95c4f8a6a4a407c4bbc1f03e8
seoan <jay2019lee@gmail.com>
2023-11-07 22:29:50 +0900

    에러로그 추가

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,15 +8,19 @@ module.exports = {
   },{
     name: 'staging-node-server',
     cwd: process.env.PM2_PROD_PATH,
-    script: './server.js',
+    script: 'node server.js',
     source: process.env.PM2_PROD_PATH,
-    post_update: ['npm install', 'pm2 restart staging-node-server']
+    post_update: ['npm install', 'pm2 restart staging-node-server'],
+    out_file: '$HOME/logs/pm2/<pid>-staging-<app name>-out.log',
+    error_file: '$HOME/logs/pm2/<pid>-staging-<app name>-error.log'
   },{
     name: 'prod-node-server',
     cwd: process.env.PM2_PROD_PATH,
     script: 'node server.js',
     source: process.env.PM2_PROD_PATH,
-    post_update: ['npm install', 'pm2 restart prod-node-server']
+    post_update: ['npm install', 'pm2 restart prod-node-server'],
+    out_file: '$HOME/logs/pm2/<pid>-prod-<app name>-out.log',
+    error_file: '$HOME/logs/pm2/<pid>-prod-<app name>-error.log'
   }]
 };
 
@@ -38,6 +42,5 @@ module.exports = {
 //   }]
 // };
 
-// out_file: 
-// error_file: 
-// pid_file: 
+// out_file: '$HOME/logs/pm2/<pid>-<app name>-out.log'
+// error_file: '$HOME/logs/pm2/<pid>-<app name>-error.log'
