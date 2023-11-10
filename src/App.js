a61d46f32c28cc274eb0bdf082865bdbaa5726e3
seoan <jay2019lee@gmail.com>
2023-11-10 18:02:14 +0900

    error page 추가

--- src/App.js
+++ src/App.js
@@ -16,32 +16,22 @@ export default function App() {
   const ready = process.env.REACT_APP_IS_READY;
 
   return (
-    (ready === 'y') ? (
-      <>
-        <BrowserRouter basename="/">
-          <Routes>
-            <Route exact path="/" element={<Layout />}>
-              <Route index element={<Home />}></Route>
-              <Route path="/home" element={<Home />}></Route>
-              <Route path="/handle-open-api" element={<OpenApiPage />}></Route>
-              <Route path="/handle-rest-api" element={<RestApiPage />}></Route>
-              <Route path="/about" element={<About />}></Route>
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route exact path="/" element={<Layout />}>
+            <Route index element={<Home />}></Route>
+            <Route path="/home" element={<Home />}></Route>
+            <Route path="/handle-open-api" element={<OpenApiPage />}></Route>
+            <Route path="/handle-rest-api" element={<RestApiPage />}></Route>
+            <Route path="/about" element={<About />}></Route>
 
-              <Route path="/calendar" element={<Calendar />}></Route>
-              <Route path="/svg-component" element={<SvgImg />}></Route>
-            </Route>
-            <Route path="*" element={<h1>Error 404</h1>}></Route>
-          </Routes>
-        </BrowserRouter>
-      </>
-    ) : (
-      <>
-        <BrowserRouter basename="/">
-          <Routes>
-            <Route path="*" element={<h1 style={{textAlign: 'center'}}>👩‍🌾 현재 작업중입니다.</h1>}></Route>
-          </Routes>
-        </BrowserRouter>
-      </>
-    )
+            <Route path="/calendar" element={<Calendar />}></Route>
+            <Route path="/svg-component" element={<SvgImg />}></Route>
+          </Route>
+          <Route path="*" element={<h1>Error 404</h1>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
